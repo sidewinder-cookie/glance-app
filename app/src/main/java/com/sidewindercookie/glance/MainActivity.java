a1161b878096a4c313689007d5e05b0e3a93d70a
Amish Shah <amishshah.2k@gmail.com>
2018-02-20 11:15:32 +0000

    yes

--- app/src/main/java/com/sidewindercookie/glance/MainActivity.java
+++ app/src/main/java/com/sidewindercookie/glance/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sidewindercookie.glance;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, NoteMaker.class);
+                startActivity(intent);
             }
         });
     }
